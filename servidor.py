716e8f5a9fb35f5c26678482bdd475ac6af3679f
PlasmaX-115 <83971348+PlasmaX-115@users.noreply.github.com>
2022-10-25 09:59:57 -0500

    Funcionalidad en el servidor para lectura de archivos

--- servidor.py
+++ servidor.py
@@ -1,4 +1,9 @@
+from stat import filemode
 from flask import Flask, request, jsonify, render_template
+from werkzug.utils import secure_filename
+from joblib import load
+import numpy as np
+import os
 
 #Generar el servidor en flask (Back-end)
 
@@ -10,6 +15,31 @@ servidorWeb = Flask(__name__) #Flask recibe la conexión
 
 def formulario():
     return render_template('pagina.html')
+
+#Procesar datos a través del Form 
+@servidorWeb.route('/modeloIA', methods=["POST"])
+def modeloForm():
+    #Procesar datos de entrada
+
+    contenido = request.Form
+    print(contenido)
+    #Convertir un diccionario a json. Será lo que regresaremos
+    return jsonify({"Resultado":"datos recibidos"})
+
+
+#Procesar datos de un archivo 
+@servidorWeb.route('/modeloFile', methods = ['POST'])
+def modeloFile():
+    f= request.files['file']
+    filename= secure_filename(f.filename)
+    path=os.path.join(os.getcwd, 'files', filename)
+    f.save(path)
+    file=open(path,'r') #Se abre un archivo en modo de lectura y lo recorre por cada línea.
+    for line in file:
+        print(line)
+    return jsonify({"Resultado":"datos recibidos"})
+
+
 
 if __name__ == '__main__':
     servidorWeb.run(debug=False, host = '0.0.0.0', port = '8080') # Dirección General
